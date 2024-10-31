c3b8cb3fc33ce54d5dfa83337030c650ab963b87
Zhu Jiarui <55539684+jiarui0000@users.noreply.github.com>
2024-10-30 23:21:11 -0400

    add token silence refresh logic

--- middleware.py
+++ middleware.py
@@ -11,7 +11,11 @@ class AccessMiddleware():
         super().__init__()
         self.app = app
         self.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
-        self.token_service = TokenService(secret_key=self.secret_key)
+        self.token_service = TokenService(
+            secret_key=self.secret_key,
+            jwt_expiration=int(os.getenv('JWT_EXPIRATION', '7200')),
+            jwt_refresh=int(os.getenv('JWT_REFRESH', '3600'))
+        )
 
     # To pass in any condition
     # def __call__(self, environ, start_response):
@@ -22,7 +26,10 @@ class AccessMiddleware():
         if token:
             result = self.token_service.verify_token(token)
             if result.status == VALIDATE_SUCCESS:
-                return self.app(environ, start_response)
+                new_token = self.token_service.refresh_token(token)  # refresh token if needed
+                response = Response.from_app(self.app, environ, start_response)
+                response.set_cookie('token', new_token)
+                return response(environ, start_response)
             else:
                 res = Response(result.get_json_result(), mimetype= 'text/plain', status=401)
                 return res(environ, start_response)
